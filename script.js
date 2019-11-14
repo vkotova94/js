36aaf56028a43478385b3a9915689b836a7cfa11
vkotova94 <vkotova94@yandex.ru>
2019-11-14 20:27:53 +0300

    done

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 function isThereACat(animals) {
     animals = animals.toLowerCase().indexOf("cat");
     if (animals !== -1) {
-        console.log('true')
-    } else {
         console.log('false')
+    } else {
+        console.log('true')
     }
 }
 isThereACat('dog,zebra,Cat,frog');
@@ -19,10 +19,25 @@ isThereACat('dog,zebra,frog');
      b = parseInt(b);
      result = Math.sqrt(a*a + b*b);
      if (c == result) {
-        console.log ('true')
+        console.log('true')
     } else {
         console.log('false')
     }
  }
  isPythagoreanTheorem(6, 8, 10);
  isPythagoreanTheorem(2, 8, 6);
+
+
+function gentleSum(a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    if (isFinite(a) && isFinite(b)) {
+        console.log(a + b);
+    } else {
+        console.log('Please enter valid data');
+    }
+}
+
+gentleSum('123asd3asd3asd', 5);
+gentleSum('Hello Wold', 5);
+gentleSum('222as', '1dasd');
